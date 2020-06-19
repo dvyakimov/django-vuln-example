e89bd1b03a466fdfd0ecf1df7837a3663419d98e
dvyakimov <denis.yakimov28@mail.ru>
2020-06-19 17:52:16 +0300

    Initial commit

---
+++ somesite/blog_views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from .models import Post
+
+# Create your views here.
+
+
+def home(request):
+    context = {
+        'posts': Post.objects.all()
+    }
+    return render(request, 'home.html', context)
+
+def about(request):
+    return render(request, 'about.html', {'title': 'About'})
